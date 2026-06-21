851efe1fa57d575f77df9d0141a5bcee3346ccec
Ansh Singh <ianshsingh10@gmail.com>
2026-06-21 17:49:55 +0530

    cors change

--- backend/config.py
+++ backend/config.py
@@ -13,7 +13,7 @@ PATROL_SCHEDULE_FILE  = DATA_DIR / "patrol_schedule.json"
 ZONES_FILE            = DATA_DIR / "df_zones.csv"
 CLUSTERS_FILE         = DATA_DIR / "df_clusters.csv"
 
-CORS_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000","https://park-alert-beyond-infinity.vercel.app/"]
+CORS_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000","https://park-alert-beyond-infinity.vercel.app"]
 
 API_HOST = "0.0.0.0"
 API_PORT = 5000
